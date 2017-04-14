2b8cc34a6d49dd7205a1766ea0c23ab2ba24e1aa
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-14 16:49:44 +0100

    Added van file and relevant Rspec tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,8 +1,6 @@
 require 'dockingstation'
-#require 'bike'
 
 describe DockingStation do
-  # bike =Bike.new
 
   describe '#dock' do
     it {is_expected.to respond_to(:dock).with(1).argument }
@@ -12,7 +10,6 @@ describe DockingStation do
       subject.dock(bike)
       dock_bikes = subject.dock(:bike)
       expect(dock_bikes).to be
-      # expect(subject.dock double(:bike)).to eq [double(:bike)]
     end
     it 'returns a bike that has been docked' do
       bike = double(:bike)
@@ -36,9 +33,6 @@ describe DockingStation do
       subject.dock double(:bike, broken?: false, working?: true )
       bike = subject.release_bike
       expect(bike).to be_working
-    #  allow(bike).to receive(:working?).and_return(true)
-    #  subject.dock(bike)
-    #  expect(subject.release_bike).to be
     end
     it 'doesnt release a broken bike' do
       bike = double(:bike)
@@ -51,9 +45,6 @@ describe DockingStation do
 
   describe "#return_bikes" do
         it { is_expected.to respond_to :return_bike }
-        # it "returns broken and working bikes" do
-        #   expect{ subject.return_bike}.to eq [bike]
-        # end
         it 'raises an error when all the bikes are returned' do
           (subject.capacity).times { subject.return_bike double(:bike) }
           expect{subject.return_bike double(:bike)}.to raise_error(RuntimeError)
