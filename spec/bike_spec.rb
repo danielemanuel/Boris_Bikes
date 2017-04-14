d318591f3819e9cf2d433225b7fe8dbae90b6578
danielemanuel <daniel.costea@vkernel.ro>
2017-04-14 13:26:56 +0100

    Bike instance replaced eith double

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,12 +1,12 @@
-require 'bike'
-
-describe Bike do
-  it {is_expected.to respond_to :working?}
-  it {is_expected.to be_working}
-
-  it 'allows bike to be broken' do
-    subject.report_broken
-    expect(subject).to be_broken
-  end
-
-end
+# require 'bike'
+#
+# describe Bike do
+#   it {is_expected.to respond_to :working?}
+#   it {is_expected.to be_working}
+#
+#   it 'allows bike to be broken' do
+#     subject.report_broken
+#     expect(subject).to be_broken
+#   end
+#
+# end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,21 +1,21 @@
 require 'dockingstation'
-require 'bike'
+#require 'bike'
 
 describe DockingStation do
-  bike = Bike.new
+  # bike =Bike.new
 
   describe '#dock' do
     it {is_expected.to respond_to(:dock).with(1).argument}
     it 'docks a bike' do
-      expect(subject.dock(bike)).to eq [bike]
+      expect(subject.dock double(:bike)).to eq [double(:bike)]
     end
     it 'returns a bike that has been docked' do
-      subject.dock(bike)
-      expect(subject.bikes).to eq [bike]
+      subject.dock double(:bike)
+      expect(subject.bikes).to eq [double(:bike)]
     end
     it 'raises an error when dockingstation is full' do
-      (subject.capacity).times { subject.dock(bike) }
-      expect{subject.dock(bike)}.to raise_error(RuntimeError)
+      (subject.capacity).times { subject.dock double(:bike) }
+      expect{subject.dock double(:bike)}.to raise_error(RuntimeError)
     end
   end
 
@@ -25,12 +25,12 @@ describe DockingStation do
       expect{subject.release_bike}.to raise_error(RuntimeError)
     end
     it 'releases a bike that works' do
-      expect(bike).to be_instance_of(Bike)
-      expect(bike).to be_working
+      expect double(:bike).to be_instance_double_of (:bike)#
+      expect double(:bike).to be_working
     end
     it 'doesnt release a broken bike' do
-      bike.report_broken
-      subject.dock(bike)
+      double(:bike).report_broken
+      subject.dock double(:bike)
       expect{subject.release_bike}.to raise_error(RuntimeError)
     end
   end
@@ -41,8 +41,8 @@ describe DockingStation do
         #   expect{ subject.return_bike}.to eq [bike]
         # end
         it 'raises an error when all the bikes are returned' do
-          (subject.capacity).times { subject.return_bike(bike) }
-          expect{subject.return_bike(bike)}.to raise_error(RuntimeError)
+          (subject.capacity).times { subject.return_bike double(:bike) }
+          expect{subject.return_bike double(:bike)}.to raise_error(RuntimeError)
         end
 
     end
